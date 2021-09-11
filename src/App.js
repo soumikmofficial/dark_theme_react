c9727954bc9dda271db105b62074663deeb84f6c
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-09-12 01:02:35 +0530

    thems working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import "./App.scss";
+import "./App.css";
 import data from "./data.js";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Article from "./components/Article.jsx";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+  const handleClick = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    console.log(new Date());
+  }, [theme]);
   return (
-    <div className="App">
-      {data.map((article) => {
-        return <h1>{article.snippet}</h1>;
-      })}
-    </div>
+    <main className="App">
+      <nav className="nav">
+        <h1>Themed</h1>
+        <button className="toggler" onClick={handleClick}>
+          {theme === "light-theme" ? "Dark" : "Light"}
+        </button>
+      </nav>
+      <section className="articles">
+        <div className="container">
+          {data.map((article) => {
+            return <Article key={article.id} {...article} />;
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
